fb246ff06435f6b011a3c0e51c667863842973f1
HaLTeX1 <pracni@gmail.com>
2024-04-14 22:41:39 +0200

    Felülírás

--- Reservation.py
+++ Reservation.py
@@ -5,37 +5,13 @@ class Reservation:
         self.data = data
 
     def reservationForm(self):
-        city = input("Kérlek, add meg hova utaznál (Település): ")
-        person_count = int(input("Kérlek, add meg hány fővel utazol: "))
-        found = False
-        hotels_in_city = []  # Lista az összes szállodára a városban
-        for hotel in self.data:
-            if hotel['location'] == city:
-                hotels_in_city.append(hotel['name'])  # Hozzáadja a szállodát a listához
-                one_bedroom_available = False
-                for room in hotel["capacity"]:
-                    if room["room_type"] == "egyágyas" and room["available_rooms"] >= person_count:
-                        one_bedroom_available = True
-                        break
-                if one_bedroom_available:
-                    print("Van szabad egyágyas szoba a kiválasztott szálláshelyen!")
-                    found = True
-                    break
-        if not found:
-            if not hotels_in_city:
-                print('A megadott településen nem tartunk nyílván szálláshelyeket!')
-            else:
-                print('Nincs szabad egyágyas szoba a kiválaszott szálláshelyen', person_count, 'fő részére!')
-        if hotels_in_city:  # Ha van találat, akkor kiírjuk a szállodák listáját
-            print("Az összes szálláshely a városban:", hotels_in_city)
+        print()
 
 
-# Load data from JSON file
-with open("Hotels.json") as f:
-    data = json.load(f)
 
 # Create an instance of the Reservation class and pass the data
-reservation = Reservation(data)
+reservation = Reservation()
 
 # Call the reservationForm method on the instance
 reservation.reservationForm()
+
--- RoomDetails.py
+++ RoomDetails.py
@@ -17,8 +17,19 @@ class SingleRoom(Room):
     def print_info(self):
         print(f"A szoba száma: {self._roomNumber}, az ár: {self._price}")
 
+class DoubleRoom(Room):
+    def __init__(self, price, roomNumber):
+        super().__init__(price, roomNumber)
+
+    def print_info(self):
+        print(f"A szoba száma: {self._roomNumber}, az ár: {self._price}")
+
 SingleRoom_Price = random.randrange(25000, 35000)
+DoubleRoom_Price = random.randrange(31200, 42500)
 
 # Példa használat:
 room = SingleRoom(SingleRoom_Price, 65)
 room.print_info()
+
+dRoom = DoubleRoom(DoubleRoom_Price, 48)
+dRoom.print_info()
